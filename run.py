9614aa8d57d1e2fa0228af20072452079c838fad
Jim Fingal <jim.fingal@gmail.com>
2015-07-21 22:42:25 -0700

    Run on average every 5 hours

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+import logging
+import random
+import sys
+
 import redis
 from twython import Twython
 
@@ -10,6 +14,11 @@ from config import (redis_url,
 
 
 if __name__ == "__main__":
+
+    rand_choice = random.randint(1, 5)
+    if rand_choice != 5:
+        logging.info("Coin flipped to %s, skipping this time." % rand_choice)
+        sys.exit()
 
     r = redis.from_url(redis_url)
     
